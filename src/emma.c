ee9ba2f27c4d2819742b637fe9c2c9ac0c031605
Ian C <ianc@noddybox.co.uk>
2003-12-17 01:36:18 +0000

    This commit was generated by cvs2svn to compensate for changes in r2, which included commits to RCS files with non-trunk default branches.

---
+++ src/emma.c
@@ -0,0 +1,90 @@
+/*
+
+    emma - Z80 testbed
+
+    Copyright (C) 2003  Ian Cowburn (ianc@noddybox.demon.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    -------------------------------------------------------------------------
+
+*/
+static const char id[]="$Id$";
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "z80.h"
+
+
+/* ---------------------------------------- GLOBALS
+*/
+static Z80Byte	mem[0x10000];
+
+
+/* ---------------------------------------- PROTOS
+*/
+static Z80Byte	ReadMem(Z80 *z80, Z80Word addr);
+static void	WriteMem(Z80 *z80, Z80Word addr, Z80Byte val);
+static Z80Byte	ReadPort(Z80 *z80, Z80Word addr);
+static void	WritePort(Z80 *z80, Z80Word addr, Z80Byte val);
+
+
+/* ---------------------------------------- MAIN
+*/
+int main(int argc, char *argv[])
+{
+    Z80 *z80;
+
+    z80=Z80Init(WriteMem,ReadMem,WritePort,ReadPort);
+
+    if (!z80)
+    {
+    	printf("Failed to initialise Z80\n");
+	return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+/* ---------------------------------------- MEMORY
+*/
+static Z80Byte ReadMem(Z80 *z80, Z80Word addr)
+{
+    return mem[addr];
+}
+
+
+static void WriteMem(Z80 *z80, Z80Word addr, Z80Byte val)
+{
+    mem[addr]=val;
+}
+
+
+static Z80Byte ReadPort(Z80 *z80, Z80Word addr)
+{
+    printf("Read from port 0x%4.4x\n",(int)addr);
+    return 0;
+}
+
+
+static void WritePort(Z80 *z80, Z80Word addr, Z80Byte val)
+{
+    printf("Wrote 0x%2.2x to port 0x%4.4x\n",(int)val,(int)addr);
+}
+
+
+/* END OF FILE */
---
+++ src/ezx81.c
@@ -0,0 +1,48 @@
+/*
+
+    ezx81 - X11 ZX81 emulator
+
+    Copyright (C) 2003  Ian Cowburn (ianc@noddybox.demon.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    -------------------------------------------------------------------------
+
+*/
+static const char id[]="$Id$";
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "z80.h"
+
+
+/* ---------------------------------------- PROTOS
+*/
+
+
+/* ---------------------------------------- MAIN
+*/
+int main(int argc, char *argv[])
+{
+    Z80 *z80;
+
+    z80=Z80Init(NULL,NULL,NULL,NULL);
+
+    return EXIT_SUCCESS;
+}
+
+
+/* END OF FILE */
